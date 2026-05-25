1848593b68d4aff491a466f1742fb799f995bde7
AnkitaDubey04 <168834958+AnkitaDiary@users.noreply.github.com>
2026-05-25 16:45:14 +0530

    Update Accelerometer_landing_to_trusted.py

--- Accelerometer_landing_to_trusted.py
+++ Accelerometer_landing_to_trusted.py
@@ -22,14 +22,14 @@ job.init(args['JOB_NAME'], args)
 customer_trusted_node1779687253043 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_trusted", transformation_ctx="customer_trusted_node1779687253043")
 
 # Script generated for node Accelerometer_landing
-Accelerometer_landing_node1779687145470 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="accelerometer_landing", transformation_ctx="Accelerometer_landing_node1779687145470")
+Accelerometer_landing_node1779687145470 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-s3-bucket-user/accelerometer/landing/"], "recurse": True}, transformation_ctx="Accelerometer_landing_node1779687145470")
 
 # Script generated for node SQL Query
-SqlQuery654 = '''
+SqlQuery0 = '''
 select distinct accelerometer_landing.* FROM accelerometer_landing
 inner join customer_trusted ON  accelerometer_landing.user = customer_trusted.email
 '''
-SQLQuery_node1779687289690 = sparkSqlQuery(glueContext, query = SqlQuery654, mapping = {"customer_trusted":customer_trusted_node1779687253043, "accelerometer_landing":Accelerometer_landing_node1779687145470}, transformation_ctx = "SQLQuery_node1779687289690")
+SQLQuery_node1779687289690 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"customer_trusted":customer_trusted_node1779687253043, "accelerometer_landing":Accelerometer_landing_node1779687145470}, transformation_ctx = "SQLQuery_node1779687289690")
 
 # Script generated for node Accelerometer_trusted
 Accelerometer_trusted_node1779687680664 = glueContext.getSink(path="s3://stedi-s3-bucket-user/accelerometer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="Accelerometer_trusted_node1779687680664")
