41e3de6fae5cc60fae17a962c13174de21b65b39
AnkitaDubey04 <168834958+AnkitaDiary@users.noreply.github.com>
2026-05-25 16:46:54 +0530

    Update Step_Trainer_landing_to_trusted.py

--- Step_Trainer_landing_to_trusted.py
+++ Step_Trainer_landing_to_trusted.py
@@ -22,14 +22,14 @@ job.init(args['JOB_NAME'], args)
 customer_curated_node1779690170348 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="customer_curated", transformation_ctx="customer_curated_node1779690170348")
 
 # Script generated for node step_trainer_landing
-step_trainer_landing_node1779689723880 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_landing", transformation_ctx="step_trainer_landing_node1779689723880")
+step_trainer_landing_node1779689723880 = glueContext.create_dynamic_frame.from_options(format_options={"multiLine": "false"}, connection_type="s3", format="json", connection_options={"paths": ["s3://stedi-s3-bucket-user/step_trainer/landing/"], "recurse": True}, transformation_ctx="step_trainer_landing_node1779689723880")
 
 # Script generated for node SQL Query
-SqlQuery900 = '''
+SqlQuery0 = '''
 select step_trainer_landing.* from step_trainer_landing inner join customer_curated on step_trainer_landing.serialnumber = customer_curated.serialnumber
 
 '''
-SQLQuery_node1779690237564 = sparkSqlQuery(glueContext, query = SqlQuery900, mapping = {"step_trainer_landing":step_trainer_landing_node1779689723880, "customer_curated":customer_curated_node1779690170348}, transformation_ctx = "SQLQuery_node1779690237564")
+SQLQuery_node1779690237564 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer_landing":step_trainer_landing_node1779689723880, "customer_curated":customer_curated_node1779690170348}, transformation_ctx = "SQLQuery_node1779690237564")
 
 # Script generated for node Step_Trainer_Trusted
 Step_Trainer_Trusted_node1779690526704 = glueContext.getSink(path="s3://stedi-s3-bucket-user/step_trainer/trusted/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="Step_Trainer_Trusted_node1779690526704")
